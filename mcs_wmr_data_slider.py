3e1830943d5cc245d947743445f7244737df429b
Kihiu Kiragu <kihiu.kiragu@quatrixglobal.com>
2024-12-25 23:25:19 +0300

    Updated Bokeh to have sliders and updating data table

--- mcs_wmr_data_slider.py
+++ mcs_wmr_data_slider.py
@@ -19,6 +19,9 @@ q_slider = Slider(title='Ventilation Rate (Q) (m3/min)',value=Q,start=1,end=1000
 v_slider = Slider(title='Emission Room Volume (V) (m3)',value=V,start=1,end=1000)
 i_slider = Slider(title='Iterations for Monte Carlo',value=iterations,start=1,end=1000)
 
+data = dict(concentrations = list(range(0, 1)))
+source = ColumnDataSource(data)
+
 # Well-Mixed Room Model
 def well_mixed_room(G, Q, V):
     """
@@ -67,66 +70,32 @@ def monte_carlo_simulation(func, params, distributions, iterations=10000):
 
     return np.array(results), results_dict
 
-def update_g(attr, old, new):
-    G = float(new)
+def update_data(attrname, old, new):
+
+    # Get the current slider values
+    G = g_slider.value
+    Q = q_slider.value
+    V = v_slider.value
+    iterations = i_slider.value
+
+    # Generate the new curve
     params = {'Q': Q, 'V': V}  # Use input values for Q and V
     distributions = {'G': stats.norm(loc=G, scale=2)}  # Example: G follows a normal distribution
     results, results_dict = monte_carlo_simulation(well_mixed_room, params, distributions, iterations)
-    update_table(results)
 
-def update_q(attr, old, new):
-    Q = float(new)
-    params = {'Q': Q, 'V': V}  # Use input values for Q and V
-    distributions = {'G': stats.norm(loc=G, scale=2)}  # Example: G follows a normal distribution
-    results, results_dict = monte_carlo_simulation(well_mixed_room, params, distributions, iterations)
-    update_table(results)
+    data = dict(concentrations = results)
 
-def update_v(attr, old, new):
-    V = float(new)
-    params = {'Q': Q, 'V': V}  # Use input values for Q and V
-    distributions = {'G': stats.norm(loc=G, scale=2)}  # Example: G follows a normal distribution
-    results, results_dict = monte_carlo_simulation(well_mixed_room, params, distributions, iterations)
-    update_table(results)
+    source.data = dict(data)
 
-def update_i(attr, old, new):
-    iterations = int(new)
-    params = {'Q': Q, 'V': V}  # Use input values for Q and V
-    distributions = {'G': stats.norm(loc=G, scale=2)}  # Example: G follows a normal distribution
-    results, results_dict = monte_carlo_simulation(well_mixed_room, params, distributions, iterations)
-    #print(f'results: {results}; \n results_dict: {results_dict}')
-    update_table(results)
-
-
-def update_table(new_results):
-    df = pd.DataFrame({'Concentration': new_results})
-    columns = [TableColumn(field=Ci, title=Ci) for Ci in df.columns]
-    #print(f'df.head():\n {df.head()}')
-    #print(f'ColumnDataSource(df): \n{ColumnDataSource(df)}')
-    data_table = DataTable(columns=columns, source=ColumnDataSource(df), width=400, height=280)
-    create_curdoc(data_table)
-
-
-def create_curdoc(data_table):
-    curdoc().clear()
-    # g_slider.value = G
-    # q_slider.value = Q
-    # v_slider.value = V
-    # i_slider.value = iterations
-    curdoc().add_root(column(g_slider, q_slider, v_slider, i_slider, data_table))
-
+for w in [g_slider, q_slider, v_slider, i_slider]:
+    w.on_change('value', update_data)
 
 params = {'Q': Q, 'V': V}  # Use input values for Q and V
 distributions = {'G': stats.norm(loc=G, scale=2)}  # Example: G follows a normal distribution
 results, results_dict = monte_carlo_simulation(well_mixed_room, params, distributions, 1)
-df = pd.DataFrame({'Concentration': results})
-columns = [TableColumn(field=Ci, title=Ci) for Ci in df.columns] # bokeh columns
-data_table = DataTable(columns=columns, source=ColumnDataSource(df), width=400, height=280) # bokeh table
 
-# Change events
-g_slider.on_change('value', update_g)
-q_slider.on_change('value', update_q)
-v_slider.on_change('value', update_v)
-i_slider.on_change('value', update_i)
+columns = [TableColumn(field="concentrations", title="concentrations")]
+data_table = DataTable(columns=columns, source=source, width=400, height=280) # bokeh table
 
 #Setting up what to display
 curdoc().add_root(column(g_slider, q_slider, v_slider, i_slider, data_table))
